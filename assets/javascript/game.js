e7828d040ffb2554768e068f558ec15885beea81
Dark Phoenix <26391122+christytreviranus@users.noreply.github.com>
2019-05-31 17:31:26 -0700

    Base files added & Base Page Design completed w/o Custom Styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Declare Variables
+
+//Wins starts at zero and increments with every win over the computer
+
+//Losses start at zero and increments with every loss to the the computer
+
+//Total Guesses is 10, so this will decrement with every guess
+
+//Guesses so far will store the user input and display each key press until a win or loss
+
+//After a win or loss the game will reset without a page refresh
