42b6f090520c41d86595dfadcae30537599a3330
Dark Phoenix <26391122+christytreviranus@users.noreply.github.com>
2019-06-03 23:12:50 -0700

    JS added for game play, background image added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,55 @@
-//Declare Variables
+//Declare variables to be used
 
-//Wins starts at zero and increments with every win over the computer
+const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let wins = 0;
+let losses = 0;
+let guessesLeft = 10;
+let guessedLetters = [];
 
-//Losses start at zero and increments with every loss to the the computer
+//Randomly generated computer guess
 
-//Total Guesses is 10, so this will decrement with every guess
+let pcGuess = chars[Math.floor(Math.random() * chars.length)];
 
-//Guesses so far will store the user input and display each key press until a win or loss
+//Guesses Left Function
 
-//After a win or loss the game will reset without a page refresh
+function countGuessesLeft(){
+	document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+//Display All User Guesses Function
+
+function userGuesses(){
+	document.querySelector('#currentGuesses').innerHTML = "Your Guesses So Far: " + guessedLetters.join(' , ');
+}
+
+//Reset Game Function
+
+const reset = function() {
+    guessesLeft = 10;
+	guessedLetters = [];
+	pcGuess = chars[Math.floor(Math.random() * chars.length)];
+	document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+	document.querySelector('#currentGuesses').innerHTML = "Your Guesses So Far: None ";
+	document.querySelector('#pc-pick').innerHTML = "???";
+}
+
+//Gameplay
+
+document.onkeyup = function(event) {
+	const userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	guessedLetters.push(userGuess);
+	userGuesses();
+	guessesLeft--;
+	countGuessesLeft();
+	
+	if (userGuess === pcGuess) {
+		wins++;
+		document.querySelector('#wins').innerHTML = "Wins: " + wins;
+		reset();
+	} 
+	else if(guessesLeft === 0) {
+		losses++;
+		document.querySelector('#losses').innerHTML = "Losses: " + losses;
+		reset();
+	}	
+}
